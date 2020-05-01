9cbb4157c12a98a337cb8dca14b85d779d361670
digi-chris <chris@barnard.tech>
2020-05-01 11:15:05 +0100

    Set up tests

---
+++ LDCTest/Tests/StringTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class StringTests
+    {
+        [Fact]
+        public void NoNulls()
+        {
+
+        }
+
+        [Fact]
+        public void LengthExceeded()
+        {
+        }
+
+        [Fact]
+        public void SpecialChars()
+        {
+        }
+
+        [Fact]
+        public void NoDuplicates()
+        {
+        }
+    }
+}
