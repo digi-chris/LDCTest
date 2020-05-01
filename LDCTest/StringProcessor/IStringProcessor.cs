49d19beed01476c83f259c9ef1aa6c5b25fd4505
digi-chris <chris@barnard.tech>
2020-05-01 12:41:05 +0100

    Create StringProcessor interface

--- LDCTest/StringProcessor/IStringProcessor.cs
+++ LDCTest/StringProcessor/IStringProcessor.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace StringProcessor
 {
-    class IStringProcessor
+    public interface IStringProcessor
     {
+        List<string> Process(List<string> stringCollection);
     }
 }
