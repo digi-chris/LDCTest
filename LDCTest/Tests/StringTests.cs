a55dbefe9bbd2280c2de5d1d2602b0029e778f4e
digi-chris <chris@barnard.tech>
2020-05-01 12:41:13 +0100

    Basic tests

--- LDCTest/Tests/StringTests.cs
+++ LDCTest/Tests/StringTests.cs
@@ -1,29 +1,57 @@
 using System;
 using Xunit;
+using StringProcessor;
+using System.Linq;
 
 namespace Tests
 {
     public class StringTests
     {
+        IStringProcessor processor;
+
+        public StringTests()
+        {
+        }
+
         [Fact]
         public void NoNulls()
         {
-
+            foreach (string processed in processor.Process(null))
+            {
+                Assert.NotNull(processed);
+            }
         }
 
         [Fact]
         public void LengthExceeded()
         {
+            foreach (string processed in processor.Process(null))
+            {
+                Assert.False(processed.Length > 15);
+            }
         }
 
         [Fact]
         public void SpecialChars()
         {
+            foreach (string processed in processor.Process(null))
+            {
+                Assert.False(processed.IndexOf('$') > -1);
+                Assert.False(processed.IndexOf('_') > -1);
+                Assert.False(processed.IndexOf('4') > -1);
+            }
         }
 
         [Fact]
         public void NoDuplicates()
         {
+            foreach (string processed in processor.Process(null))
+            {
+                if (processed.Length > 1)
+                {
+                    Assert.False(processed.Where((c, i) => i > 1 && processed[i - 1] == c).Any());
+                }
+            }
         }
     }
 }
