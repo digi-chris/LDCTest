aa92a99313d0f1a8c0f77ef7d315f2c2d80c060f
digi-chris <chris@barnard.tech>
2020-05-03 11:51:57 +0100

    Add comments

--- LDCTest/StringProcessor/LDCStringProcessor.cs
+++ LDCTest/StringProcessor/LDCStringProcessor.cs
@@ -12,11 +12,21 @@ namespace StringProcessor
         {
             List<string> processedStrings = new List<string>();
 
+            // Run through the list of strings for processing
             foreach (string item in stringCollection)
             {
                 if (item != null)
                 {
-                    //char? lastLetter = null;
+                    // run through each char in each individual string
+
+                    // NOTE: I feel like this is a simplistic approach that probably doesn't reflect how I would
+                    // usually code in a production setting. They are probably other ways of doing this, using Regular
+                    // Expressions and more intelligent string manipulation functions. However, I generally like
+                    // to write in a way that's most appropriate for a codebase, and allows additional functionality
+                    // to be added. In this case, since I don't have a bigger picture to think of, I thought it
+                    // best to write the code in a way that was simple and easy-to-read. This would then be considered
+                    // a first step towards iteratively improving the method.
+
                     string output = "";
                     foreach (char letter in item)
                     {
